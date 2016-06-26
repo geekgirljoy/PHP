0c00463f256c6575d33bc037e01dd6028b296e11
Joy Harvel <geekgirl.joy@gmail.com>
2016-06-26 12:05:50 -0700

    Updated CheckForPalindromes.php
    Moved the tests into an array and used a foreach loop to iterate over
    the test array

--- CheckForPalindromes.php
+++ CheckForPalindromes.php
@@ -1,5 +1,5 @@
 <?php
-function palindrome($str) 
+function Palindrome($str) 
 {
   $str = strtolower($str); // convert to lower
   $str = preg_replace("/[^a-zA-Z0-9]/", '', $str);
@@ -9,15 +9,22 @@ function palindrome($str)
   else{ return "true"; }
 }
 
-echo palindrome("eye") . "<br/>"; //should return true.
-echo palindrome("race car") . "<br/>"; //should return true.
-echo palindrome("not a palindrome") . "<br/>"; //should return false.
-echo palindrome("A man, a plan, a canal. Panama") . "<br/>"; //should return true.
-echo palindrome("never odd or even") . "<br/>"; //should return true.
-echo palindrome("nope") . "<br/>"; //should return false.
-echo palindrome("almostomla") . "<br/>"; //should return false.
-echo palindrome("My age is 0, 0 si ega ym.") . "<br/>"; //should return true.
-echo palindrome("1 eye for of 1 eye.") . "<br/>"; //should return false.
-echo palindrome("0_0 (: /-\ :) 0-0") . "<br/>"; //should return true.
+// Tests
+$test_arr = [
+'eye',//should return true.
+'race car',//should return true.
+'not a Palindrome',//should return false.
+'A man, a plan, a canal. Panama',//should return true.
+'never odd or even',//should return true.
+'nope',//should return false.
+'almostomla',//should return false.
+'My age is 0, 0 si ega ym.',//should return true.
+'1 eye for of 1 eye.',//should return false.
+'0_0 (: /-\ :) 0-0'//should return true.
+];
 
+
+foreach($test_arr as $str){
+	echo "$str - <b> " . Palindrome($str) . "</b><br/><br/>" . PHP_EOL;
+}
 ?>
