42fbe131580d20c58f1472ff5a45099a7a816d06
Joy Harvel <geekgirl.joy@gmail.com>
2016-06-26 11:26:33 -0700

    Correcting DecFloatOctHexBin.php

--- DecFloatOctHexBin.php
+++ DecFloatOctHexBin.php
@@ -1,13 +1,21 @@
 <?php
+header("Content-Type:text/plain"); 
 
-class StrawNeckedIbis
-{
-// Static property with visibility
-public static $numberOfBirds = 0;
-// Method with visibility
-public function __construct()
-{
-static::$numberOfBirds++;
-}
-}
+$myDec = 1;			// Decimal
+$myFloat = 1.0;		// Floating Point
+$myOct = 001;	    // Octal
+$myHex = 0x01;		// Hexadecimal
+$myBin = 0b00000001;// Binary
+
+
+echo <<<output
+
+Decimal: $myDec
+Floating Point: $myFloat
+Octal: $myOct
+Hexadecimal: $myHex
+Binary: $myBin
+
+output;
+
 ?>
