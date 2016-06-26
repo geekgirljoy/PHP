9af01c762182fd3511a42bcf5792864e82f27a4e
Joy Harvel <geekgirl.joy@gmail.com>
2016-06-26 11:46:51 -0700

    Updated AssociativeArray.php
    Minor changes to output and code formatting.

--- AssociativeArray.php
+++ AssociativeArray.php
@@ -5,17 +5,23 @@
   <body>
     <p>
       <?php
-      $myArray = array(
-                       array('fule', 300201),
-                       array('oxygen', 10034),
-                       array('name', 'Asteroid Miner 99')
+      $my_array = array(
+	  	  	  	  	   array('Name', 'Asteroid Miner 99'),
+                       array('Fule', 300201),
+                       array('Oxygen', 10034)                       
                       );
-    echo 'Echoing My Associative Array<br/>';    
-    for($i = 0; $i < sizeof($myArray); $i++)
-     {
-        foreach($myArray[$i] as $key=>$value){ echo ' ' . $value  ; }
-        echo '<br/>';
-     }
+    echo '<h3>Echoing my associative array:</h3>' . PHP_EOL;
+	
+    for($i = 0; $i < sizeof($my_array); $i++){
+		
+        foreach($my_array[$i] as $key=>$value){
+			    echo $value; 
+			}
+        echo '<br/>' . PHP_EOL;
+    }
+	
+	//print_r($my_array);
+	//var_dump($my_array);
      
       ?>
     </p>
