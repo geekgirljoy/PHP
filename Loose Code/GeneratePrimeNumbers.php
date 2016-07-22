1d183c217667bc385d3e476f32c3645cb0f22879
Joy Harvel <geekgirl.joy@gmail.com>
2016-07-22 11:59:15 -0700

    Update GeneratePrimeNumbers.php
    Removed echo from test() function and changed returned data to a built string in addition to changing from an if/else to a ternary operation to improve simplicity and readability. Added and improved comments.

--- Loose Code/GeneratePrimeNumbers.php
+++ Loose Code/GeneratePrimeNumbers.php
@@ -8,48 +8,42 @@ set_time_limit ( 3600 ); // max run time 1 hour (adjust as needed)
 ini_set('memory_limit', '-1');
 
 
-function isprime($num)
-{
-    if($num == 1){return false;}     //1 is not prime.
-    if($num == 2){return true;}      //2 is prime (the only even number that is prime)
-    if($num % 2 == 0){return false;} // If the number is divisible by two it's not prime
+function isprime($num){
+    if($num == 1){return false;}     // 1 is not prime.
+    if($num == 2){return true;}      // 2 is prime (the only even number that is prime)
+    if($num % 2 == 0){return false;} // If the number is divisible by two (therefore an even number) it's not prime
 
     //  Checks odd numbers for primality.
-    for($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2)
-    {
+    for($i = 3; $i <= ceil(sqrt($num)); $i = $i + 2){
         if($num % $i == 0){return false;}
     }
-    return true;
+    return true; // I found a prime number! :-)
 }
 
+// Is $num equal to $expected) test and report
 function test($num, $expected){
-    // test
-	echo "<br>Recieved $num.<br>Expected $expected". PHP_EOL;
-    if($num == $expected){return "<br>Passed Test" . PHP_EOL;}
-    else{return "<br>Failed Test" . PHP_EOL;}
+    $output = PHP_EOL ."<br>Recieved: $num." . PHP_EOL . "<br>Expected: $expected". PHP_EOL . "<br>Result: ";
+    $output .= ($num == $expected) ? "Passed Test" : "Failed Test";
+    return $output;
 }
 
 $min = 0;               // Number to start with
-$max = 1000000;         // Number of primes to generate (should end on the 1 millionth prime 15,485,863)
+$max = 100;             // Number of primes to generate (should end on the 1 millionth prime 15,485,863)
 $curr_num = $min;       // The current number to check if prime
 $prime_stack = array(); // Found primes are stored here
 
 echo "Generating $max Prime Numbers Beginning at: $min <br>" . PHP_EOL;
 
 
-while (count($prime_stack) < $max)
-{
-    if (isprime($curr_num))
-    {
-        echo $curr_num;
-		if (count($prime_stack) < $max-1){echo ", ";} // not last so add comma to output
-        array_push($prime_stack, $curr_num);
+while (count($prime_stack) < $max){
+    if (isprime($curr_num)){
+    	array_push($prime_stack, $curr_num); // Place the prime in the $prime_stack array
+        echo $curr_num; // all hail the new found prime!
+	if (count($prime_stack) < $max-1){echo ", ";} // not last number so echo comma because it's prettier to look at
     }
-    $curr_num++;
+    $curr_num++; // Increment the $curr_num variable so we can check the next number for primality
 }
 
-
-echo test($prime_stack[$max-1], 15485863);
-   
-   
+// All done! Do test to make sure the computed output matches our expected KGV (Known Good Value)
+echo test($prime_stack[$max-1], 15485863); // The  1,000,000 prime number is 15,485,863
 ?>
