f08439aaf3dd37756e06023c6f7c5c2668129c76
Joy Harvel <geekgirl.joy@gmail.com>
2016-08-06 00:01:25 -0700

    Update BubbleSort.php
    Converting tests to a for loop

--- Loose Code/BubbleSort.php
+++ Loose Code/BubbleSort.php
@@ -29,37 +29,10 @@ $sorted = BubbleSort(array(9,8,7,6,5,1,4,3,2,0));
 var_dump($sorted);
 
 echo '<br>' . PHP_EOL;
-// Test proofs for provided array 0 - 9
-if ($sorted[0] == 0) {
-	echo '0 is correct<br>' . PHP_EOL;
-}
-if ($sorted[1] == 1) {
-	echo '1 is correct<br>' . PHP_EOL;
-}
-if ($sorted[2] == 2) {
-	echo '2 is correct<br>' . PHP_EOL;
-}
-if ($sorted[3] == 3) {
-	echo '3 is correct<br>' . PHP_EOL;
-}
-if ($sorted[4] == 4) {
-	echo '4 is correct<br>' . PHP_EOL;
-}
-if ($sorted[5] == 5) {
-	echo '5 is correct<br>' . PHP_EOL;
-}
-if ($sorted[6] == 6) {
-	echo '6 is correct<br>' . PHP_EOL;
-}
-if ($sorted[7] == 7) {
-	echo '7 is correct<br>' . PHP_EOL;
-}
-if ($sorted[8] == 8) {
-	echo '8 is correct<br>' . PHP_EOL;
-}
-if ($sorted[9] == 9) {
-	echo '9 is correct<br>' . PHP_EOL;
-}
 
-
+for($i = 0; $i <= 9; $i++){
+	if ($sorted[$i] == $i) {
+	echo "$i is correct<br>" . PHP_EOL;
+    }
+}
 ?>
