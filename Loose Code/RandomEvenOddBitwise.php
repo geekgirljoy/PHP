81d48c4e5d8bbbeb45af6ee3718588b98dde3b9d
Joy Harvel <geekgirl.joy@gmail.com>
2017-08-10 01:00:41 -0700

    Update RandomEvenOddBitwise.php

--- Loose Code/RandomEvenOddBitwise.php
+++ Loose Code/RandomEvenOddBitwise.php
@@ -2,6 +2,11 @@
     // http://php.net/manual/en/language.operators.bitwise.php
     // https://en.wikipedia.org/wiki/Bitwise_operation
     
-    echo $even = mt_rand(2,80) & ~1; // generate random number then set the lest significant bit to 0 so that we have an even number
-    echo $odd = mt_rand(2,80) | 1;   // generate random number then set the lest significant bit to 1 so that we have an odd number
+    echo $even = mt_rand(2,80) & ~1; // generate a random number between 0 & 80 
+                                     // then set the least significant bit to 0 
+                                     // so that we have an even number.
+
+    echo $odd = mt_rand(2,80) | 1;   // generate a random number between 0 & 80 
+                                     // then set the least significant bit to 1 
+                                     // so that we have an odd number.
 ?>
