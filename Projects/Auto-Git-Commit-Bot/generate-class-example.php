a58e9ca760b1c4e94e290be96da34ba52832a372
Joy Harvel <geekgirl.joy@gmail.com>
2018-02-12 00:16:51 -0800

    Update generate-class-example.php

--- Projects/Auto-Git-Commit-Bot/generate-class-example.php
+++ Projects/Auto-Git-Commit-Bot/generate-class-example.php
@@ -1,7 +1,7 @@
 <?php
 
-echo 'Class examples can be found in the classes subfolder.';
-echo 'Support me on Patreon for $1 or more and get access to the Class Generator code.';
-echo 'Patreon: https://www.patreon.com/GeekGirlJoy';
-echo 'Much Love,';
-echo '~Joy';
+echo 'Class examples can be found in the classes subfolder.' . PHP_EOL;
+echo 'Support me on Patreon for $1 or more and get access to the Class Generator code.' . PHP_EOL;
+echo 'Patreon: https://www.patreon.com/GeekGirlJoy' . PHP_EOL;
+echo 'Much Love,' . PHP_EOL;
+echo '~Joy' . PHP_EOL;
