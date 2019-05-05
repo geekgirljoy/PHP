b50a13a6821038bfa7e0df541fc27a802d3afcbb
Joy Harvel <geekgirl.joy@gmail.com>
2019-05-05 16:46:03 -0700

    Update SplitRGB.php

--- Projects/SplitRGBChannels/SplitRGB.php
+++ Projects/SplitRGBChannels/SplitRGB.php
@@ -1,7 +1,7 @@
 <?php
 
 function SplitRGBChannels(&$img){
-    // get image size
+        // get image size
 	$width = ImagesX($img);
 	$height = ImagesY($img);
 
@@ -11,9 +11,9 @@ function SplitRGBChannels(&$img){
 	$monochrome_blue = ImageCreateTrueColor($width, $height);
 
 	// black background otherwise
-    ImageColorAllocate($monochrome_red, 0, 0, 0);
-    ImageColorAllocate($monochrome_green, 0, 0, 0);
-    ImageColorAllocate($monochrome_blue, 0, 0, 0);
+        ImageColorAllocate($monochrome_red, 0, 0, 0);
+        ImageColorAllocate($monochrome_green, 0, 0, 0);
+        ImageColorAllocate($monochrome_blue, 0, 0, 0);
 		
 	// for all the rows and columns in the input image
 	for($row = 0; $row < $width; $row++){
@@ -23,7 +23,7 @@ function SplitRGBChannels(&$img){
 			$pixel = ImageColorAt($img, $row, $col); // get the color for the pixel 
 			$colors = ImageColorsForIndex($img, $pixel); // make human readable 
              
-            // allocate the red value of the pixel to the red image and draw the pixel
+                        // allocate the red value of the pixel to the red image and draw the pixel
 			$red_color = ImageColorAllocate($monochrome_red, $colors['red'], 0, 0);
 			ImageSetPixel($monochrome_red, $row, $col, $red_color);
 			
