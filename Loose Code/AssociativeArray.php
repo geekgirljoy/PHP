a94bf614a7ba2d4b374a17dd3056a676a91f4fa2
Joy Harvel <geekgirl.joy@gmail.com>
2019-08-27 11:27:22 -0700

    Update AssociativeArray.php

--- Loose Code/AssociativeArray.php
+++ Loose Code/AssociativeArray.php
@@ -6,17 +6,16 @@
     <p>
       <?php
       $my_array = array(
-	  	  	  	  	   array('Name', 'Asteroid Miner 99'),
+	  	       array('Name', 'Asteroid Miner 99'),
                        array('Fule', 300201),
                        array('Oxygen', 10034)                       
                       );
     echo '<h3>Echoing my associative array:</h3>' . PHP_EOL;
 	
     for($i = 0; $i < sizeof($my_array); $i++){
-		
         foreach($my_array[$i] as $key=>$value){
-			    echo $value; 
-			}
+	    echo $value; 
+	}
         echo '<br/>' . PHP_EOL;
     }
 	
