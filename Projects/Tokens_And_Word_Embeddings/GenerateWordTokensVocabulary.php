8a1baff3d22beb0c69294777bc8dca17b9453a44
geekgirljoy <geekgirl.joy@gmail.com>
2022-08-16 23:19:50 -0700

    Create GenerateWordTokensVocabulary.php

---
+++ Projects/Tokens_And_Word_Embeddings/GenerateWordTokensVocabulary.php
@@ -0,0 +1,25 @@
+<?php
+/*
+This generates the vocabulary from the text file.
+words is sourced from /usr/dict/words on Linux
+*/
+
+// fopen the file 'words' and read the contents into an array, the data is newline separated
+$words = fopen("words", "r");
+$word_array = array(); 
+$token = 0;
+
+// Loop through the file and add each word to the array 
+// as the key and the token value as the index offset beginning at 0
+while (!feof($words)) {
+    $word_array[trim(fgets($words))] = $token;
+    $token++;
+}
+fclose($words);// Close the words file
+
+$tokens = json_encode($word_array, 1); // Encode the array to json
+
+// Write the tokens to a vocab.json file
+$vocab = fopen("vocab.json", "w");
+fwrite($vocab, $tokens);
+fclose($vocab);
